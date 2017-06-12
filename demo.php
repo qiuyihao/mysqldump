ff19e135ff0e6541b1408aa4c43eeaff672e1d54
cndocker <523803250@qq.com>
2017-06-12 10:45:46 +0800

    demo code

---
+++ demo.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once (__DIR__ . '/vendor/autoload.php');
+
+use EasyMySQLDump\EasyMySQLDump;
+use EasyMySQLDump\EasySendEmail;
+
+/**
+ * 1. Mysqldump
+ */
+$easyMySQLDump = new EasyMySQLDump();
+$easyMySQLDump->setDatabase('127.0.0.1', 3306, 'root', '', 'test')
+	->setDumpFile('/web/test.mysqldump.sql')
+	->mysqldump();
+
+/**
+ * 2. Send email
+ */
+$easySendEmail = new EasySendEmail();
+$easySendEmail->debug = true;
+$sendResult = $easySendEmail
+	->setSubject('mysqldump')
+	->setBody('A mail to backup mysqldump file')
+	->setSMTP('smtp.yeah.net', 465, '[username]@yeah.net', '[password]')
+	->setSender('[sender address]', '[sender name]')
+	->setReceiver('[receiver address]')
+	->addAttachment($easyMySQLDump->getDumpFile(), 'mysqldump.sql')
+	->send();
+if ($sendResult === false) {
+	echo $easySendEmail->error;
+	exit;
+}
